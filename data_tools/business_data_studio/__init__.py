14f1c7a6522d9cf961d9389ba0aa6696f502d7e8
Hasan <123743639+Laodemeia@users.noreply.github.com>
2026-07-24 23:34:29 +0300

    Add application package

---
+++ data_tools/business_data_studio/__init__.py
@@ -0,0 +1,15 @@
+"""Business Data Automation Studio package."""
+
+from .engine import (
+    DataProcessingEngine,
+    ProcessingConfig,
+    ProcessingIssue,
+    ProcessingResult,
+)
+
+__all__ = [
+    "DataProcessingEngine",
+    "ProcessingConfig",
+    "ProcessingIssue",
+    "ProcessingResult",
+]
